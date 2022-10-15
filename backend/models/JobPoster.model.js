b72e283e752ff19b1f6045b1b6e573b21b14428f
ali-ismail02 <alii.ismail02@gmail.com>
2022-10-15 15:59:00 +0300

    Backend: Added job posters model

---
+++ backend/models/JobPoster.model.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+exports.postSchema = mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    company_name: {
+        type: String,
+        required: 'Company name is required'
+    },
+    industry: {
+        type: String,
+        required: 'industry is required'
+    },
+    company_size: {
+        type: String,
+    },
+    overview: {
+        type: String,
+    },
+    hq: {
+        type: String,
+    },
+    website: {
+        type: String,
+    },
+    locations: {
+        type: String,
+    }
+});
+
+const model = mongoose.model('User', postSchema);
+
+module.exports = model;
