17c0f7736230b99a38a0841ff7203684733824a0
ali-ismail02 <alii.ismail02@gmail.com>
2022-10-15 22:57:13 +0300

    Backend: companies controller => added add job api auth controller => Added image save

--- backend/controllers/users.controller.js
+++ backend/controllers/users.controller.js
@@ -1,11 +1,17 @@
 const User = require('../models/User.model')
 const Job = require('../models/Job.model')
+const Company = require('../models/Company.model')
 
 const getJobs = async (req, res)=>{
+    console.clear()
     const user = req.user;
     const jobs = await Job.find({available:1,location: { $in: user.info.seeking_locations },job_title: user.info.seeking}).sort({date:-1})
-    console.log(user.info.seeking.toString())
-    res.send(jobs)
+    const arr = new Array()
+    await Promise.all(jobs.map(async (job) => {
+        job.company_info = await Company.findOne(job.Company)
+        arr.push([job,job.company_info])
+    }));
+    res.send(arr)
 }
 
 module.exports = {
