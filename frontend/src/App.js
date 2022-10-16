4185762406775c581dc60a8effc7d0a061cd03c1
ali-ismail02 <alii.ismail02@gmail.com>
2022-10-16 20:12:02 +0300

    Frontend: Added input component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Input from "./components/Input";
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<AddCourse/>}/>
+        <Route path="/" element={<Input type={"text"} placeholder={"Email"} value={null} onChange={null}/>}/>
       </Routes>
     </Router>
   );
